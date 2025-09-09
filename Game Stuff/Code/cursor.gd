bb181a318c91fc58765338f7d29145ddc34cc8df
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-09-09 15:18:06 -0400

    SET UP BASIC CODE
    hit q to toggle moviugn mosue

--- Game Stuff/Code/cursor.gd
+++ Game Stuff/Code/cursor.gd
@@ -1,9 +1,32 @@
 extends Node2D
 
+var speed: int = 10
+var previous_pos: Vector2
+
+@onready var world = $".."
 @onready var mouse = $cursorSprite
+@onready var camera = $Camera2D
+@onready var ray = $cursorSprite/RayCast2D
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
 
 func _physics_process(delta: float) -> void:
-	mouse.position = mouse.get_global_mouse_position()
+	if world.game_playing:
+		if previous_pos:
+			ray.target_position = previous_pos-mouse.position
+		previous_pos = mouse.position
+		mouse.position = mouse.get_global_mouse_position()
+		ray.force_raycast_update()
+		if ray.is_colliding():
+			get_tree().reload_current_scene()
+		
+		if Input.get_action_strength("up") >= 0.5:
+			camera.position.y -= speed
+		elif Input.get_action_strength("down") >= 0.5:
+			camera.position.y += speed
+		
+		if Input.get_action_strength("left") >= 0.5:
+			camera.position.x -= speed
+		elif Input.get_action_strength("right") >= 0.5:
+			camera.position.x += speed
--- Game Stuff/Code/start.gd
+++ Game Stuff/Code/start.gd
@@ -1,4 +1,19 @@
 extends Node2D
 
+var game_playing: bool = true
+
+
+
 func _ready() -> void:
 	randomize()
+	Input.warp_mouse(get_viewport().get_visible_rect().size / 2)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("mouse_free"):
+		if game_playing:
+			game_playing = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			game_playing = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
