e497b4d6695afe45bbb1931859597aa5d0a16dfc
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-09-09 15:34:13 -0400

    ok good setup

--- Game Stuff/Code/start.gd
+++ Game Stuff/Code/start.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var game_playing: bool = true
-
+var pressed:bool = false
 
 
 func _ready() -> void:
@@ -10,10 +10,16 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("mouse_free"):
+	
+	if Input.get_action_strength("mouse_free") >= 0.5 and !pressed:
+		pressed = true
 		if game_playing:
 			game_playing = false
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
 		else:
 			game_playing = true
 			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	elif pressed:
+		await get_tree().process_frame
+		pressed = false
